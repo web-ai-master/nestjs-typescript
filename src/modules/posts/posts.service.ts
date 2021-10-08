226f3e9d7b3df1a06dae8eb13e7c58335d591bdb
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-08 11:34:51 +0530

    feat: Create all required service methods related to post

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Post } from './models/entities/post.entity';
+import { CreatePostDto } from './models/dto/createPost.dto';
+import { UpdatePostDto } from './models/dto/updatePost.dto';
 
 @Injectable()
 export class PostsService {
@@ -18,8 +20,34 @@ export class PostsService {
     async getPostById(id: number) {
         const post = await this.postsRepository.findOne(id);
         if (post) {
-        return post;
+            return post;
         }
         throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
-  }
+    }
+
+    /* create new post */
+    async createPost(createPostDto: CreatePostDto) {
+        const newPost = await this.postsRepository.create(createPostDto);
+        await this.postsRepository.save(newPost);
+        return newPost;
+    }
+
+    /* update post */
+    async updatePost(id: number, updatePostDto: UpdatePostDto) {
+        await this.postsRepository.update(id, updatePostDto);
+        const updatedPost = await this.postsRepository.findOne(id);
+        if (updatedPost) {
+            return updatedPost
+        }
+        throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    }
+
+    /* delete post */
+    async deletePost(id: number) {
+        const deleteResponse = await this.postsRepository.delete(id);
+        if (!deleteResponse.affected) {
+        throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
