59bec230ca5219d257b7cc41004754dccfe047eb
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-08 13:04:56 +0530

    feat: Create users modules and peering it in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './modules/posts/posts.module';
+import { UsersModule } from './modules/users/users.module';
 
 import config from '../ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), PostsModule],
+  imports: [TypeOrmModule.forRoot(config), PostsModule, UsersModule],
   controllers: [],
   providers: [],
 })
