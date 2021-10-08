7d3f83dd8c7ec1f84a313795f7834752c31a6cda
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-08 13:04:12 +0530

    feat: Create user.entity.ts

---
+++ src/modules/users/models/entities/user.entity.ts
@@ -0,0 +1,15 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+}
