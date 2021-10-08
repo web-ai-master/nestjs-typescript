8a33b90287d8ef021e26bc3d75496a0970fc86d1
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-08 16:18:32 +0530

    refactor: Refactor application code base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PostsModule } from './modules/posts/posts.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
