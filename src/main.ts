49728da505d86f258f82b661f872e85dfc50363b
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-09 13:54:23 +0530

    config: Peering ClassSerializerInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1/');
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(
+    app.get(Reflector))
+  );
   app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
   await app.listen(3000);
 }
