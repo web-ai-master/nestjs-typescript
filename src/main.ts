c89e1d963ca271da7b6bc75ccbfea11ca1f3e18c
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-09 08:50:50 +0530

    feat: Add skippingMissingFields: true global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1/');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
   await app.listen(3000);
 }
 bootstrap();
