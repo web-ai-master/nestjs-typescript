62529b73c34df3995ff3c01066d870b2d656958c
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-09 13:52:53 +0530

    feat: Create Address entity

--- src/modules/users/models/entities/user.entity.ts
+++ src/modules/users/models/entities/user.entity.ts
@@ -1,5 +1,7 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import * as bcrypt from 'bcrypt';
+import { Address } from "./address.entity";
+import { Exclude } from "class-transformer";
 
 @Entity()
 export class User {
@@ -12,8 +14,16 @@ export class User {
     @Column({ unique: true })
     email: string;
 
+    @Exclude()
     @Column()
     password: string;
+
+    @OneToOne(() => Address, {
+        eager: true, // related entities always to be included while fetched
+        cascade: true // save an address while saving a user
+    })
+    @JoinColumn()
+    public address: Address;
 
     @BeforeInsert()
     async hashPassword() {
