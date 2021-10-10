b01da12ee5ca210d90e5ed3e1c88144d6f24bb88
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-10 14:54:45 +0530

    feat: Create methods to update category

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,7 +1,10 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CategoryExistsException } from './exceptions/categoryExists.exception';
+import { CategoryNotFoundException } from './exceptions/categoryNotFound.exception';
 import { CreateCategoryDto } from './models/dto/createCategory.dto';
+import { UpdateCategoryDto } from './models/dto/updateCategory.dto';
 
 import { Category } from './models/entities/category.entity';
 
@@ -10,15 +13,49 @@ export class CategoriesService {
 
     constructor(@InjectRepository(Category) private categoriesRepository:Repository<Category>) { }
 
+    /* get categories */
+    async getAllCategories() {
+        return await this.categoriesRepository.find();
+    }
+
+    /* get single category */
+    async getCategory(id: number) {
+        const category = await this.categoriesRepository.findOne(id);
+        if (category) {
+            return category;
+        }
+        throw new CategoryNotFoundException(id);
+    }
+
     /* create new category */
     async createCategory(createCategoryDto:CreateCategoryDto) {
-        const newCategory = await this.categoriesRepository.create(createCategoryDto);
         try {
+            const newCategory = await this.categoriesRepository.create(createCategoryDto);
             await this.categoriesRepository.save(newCategory);
             return newCategory;
         } catch (err) {
+            if(err.code === 'ER_DUP_ENTRY') {
+                throw new CategoryExistsException(createCategoryDto.name)
+            } 
             throw new InternalServerErrorException(err)
-        }
-       
+        }   
+    }
+
+    /* update category */
+    async updateCategory(id: number, updateCategoryDto: UpdateCategoryDto) {
+        try {
+            const category = await this.getCategory(id);
+            if(category) {
+                await this.categoriesRepository.update(id, updateCategoryDto);
+                const updatedCategory = await this.categoriesRepository.findOne(id);
+                return updatedCategory;
+            }
+            throw new CategoryNotFoundException(id);
+        } catch (err) {
+            if(err.code === 'ER_DUP_ENTRY') {
+                throw new CategoryExistsException(updateCategoryDto.name)
+            } 
+            throw new InternalServerErrorException(err)
+        }  
     }
 }
