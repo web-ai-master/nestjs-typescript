1a7523c2016e5e5e62f2da76c20f8470c5a75ac8
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-10 10:08:16 +0530

    feat: Create Categories entity

---
+++ src/modules/categories/models/entities/category.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+ 
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+ 
+  @Column()
+  name: string;
+}
+ 
