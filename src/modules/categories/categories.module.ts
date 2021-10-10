2247d6050116acb9fddf89e4c268ee5eaa0296d2
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-10 10:35:31 +0530

    feat: Peering Category entity in category.module.ts

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesController } from './categories.controller';
+import { CategoriesService } from './categories.service';
+import { Category } from './models/entities/category.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature(
+      [Category]
+    )
+  ],
+  controllers: [CategoriesController],
+  providers: [CategoriesService]
+})
 export class CategoriesModule {}
