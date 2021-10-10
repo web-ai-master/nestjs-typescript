f6a556a94a9083577dbe8984212fd32e00453978
jovan0901 <jovanvelickovic0901@gmail.com>
2021-10-10 12:01:56 +0530

    feat: Peering catergory service & controller in category.module.ts

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 import { Category } from './models/entities/category.entity';
